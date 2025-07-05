59e310b217659ca5af7898ba6106b4b1c58ad650
Denise Noemi Rea Diaz <105187806+DeniseRea@users.noreply.github.com>
2025-07-05 15:50:17 -0500

    ADD file Preview

--- Block-chain/src/pages/Update/UpdatePage.jsx
+++ Block-chain/src/pages/Update/UpdatePage.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 import { FileInput } from "../../components/FileInput"; 
+import { FilePreview } from "../../components/FilePreview";
 
 export const UpdatePage = () => {
   const [fileContent, setFileContent] = useState("");
   const [fileName, setFileName] = useState("");
-
   
   const handleFileChange = (file) => {
     setFileName(file.name); 
@@ -25,7 +25,8 @@ export const UpdatePage = () => {
       
       <FileInput onFileChange={handleFileChange} />
 
-      
+      <FilePreview fileName={fileName} fileContent={fileContent} />
+
     </div>
   );
 }
