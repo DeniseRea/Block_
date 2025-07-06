e76640916cfae0d6ab282756db73fc6e78267b56
Denise Noemi Rea Diaz <105187806+DeniseRea@users.noreply.github.com>
2025-07-05 22:33:20 -0500

    Update Config

--- Block-chain/src/pages/Config/ConfigPage.jsx
+++ Block-chain/src/pages/Config/ConfigPage.jsx
@@ -1,5 +1,8 @@
-import { SelectInput } from "../../components/SelectInput";
+import { TableHeader } from "../../components/Table/TableHeader";
+import { TableBody } from "../../components/Table/TableBody";
+import { TableContainer } from "../../components/Table/TableContainer";
 import { InputField } from "../../components/InputField";
+import { SelectInput } from "../../components/SelectInput";
 
 export const ConfigPage = () => {
   const options = [
@@ -8,34 +11,29 @@ export const ConfigPage = () => {
     { value: "anime", label: "Anime" },
   ];
 
+  const headers = ["Inicio", "Texto", "Final"];
+
+  const data = [
+    {
+      inicio: <InputField label="" type="number" placeholder="00000" />,
+      texto: <SelectInput options={options} />,
+      final: <InputField label="" type="number" placeholder="00000" />,
+    },
+  ];
+
+  const extractValues = (item) => [item.inicio, item.texto, item.final];
+
   return (
     <div className="container py-5">
-      <h2 className="text-center mb-4">Configuración</h2>
+      <h2 className="text-center mb-4 text-primary">Configuración</h2>
+        <p>Ingrese el número de ceros a agregar:</p>
 
-      <div className="table-responsive">
-        <table className="table table-bordered text-center">
-          <thead>
-            <tr>
-              <th>Inicio</th>
-              <th>Texto</th>
-              <th>Final</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <InputField label="" type="number" placeholder="00000" />
-              </td>
-              <td>
-                <SelectInput options={options} />
-              </td>
-              <td>
-                <InputField label="" type="number" placeholder="00000" />
-              </td>
-            </tr>
-          </tbody>
+      <TableContainer>
+        <table className="table table-hover table-bordered text-center">
+          <TableHeader headers={headers} />
+          <TableBody data={data} extractValues={extractValues} />
         </table>
-      </div>
+      </TableContainer>
     </div>
   );
 };
